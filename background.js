b2e9d3561530866f7736fbe23c4ce2d5355fd44e
BeamoINT <beamo@beamosupport.com>
2023-12-12 19:02:04 -0700

    Made Basic Functionality Changes

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.get({searchEngine: 'https://beamosearch.com'}, function(items) {
+    chrome.storage.sync.set({ searchEngine: 'https://beamosearch.com/results.html?q=%s' });
+  });
+  
+  chrome.runtime.onStartup.addListener(() => {
+    chrome.storage.sync.get({ searchEngine: 'https://beamosearch.com/results.html?q=%s' }, function (items) {
       chrome.search.set({
         default: true,
         search_url: items.searchEngine
