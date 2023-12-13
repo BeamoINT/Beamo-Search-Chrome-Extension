1802272abaaa058771a6b648b66286004c1718df
BeamoINT <beamo@beamosupport.com>
2023-12-12 19:35:25 -0700

    Made More Modifications

--- options.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('save').onclick = function() {
-    var searchEngine = document.getElementById('searchEngine').value;
-    chrome.storage.sync.set({searchEngine: searchEngine}, function() {
-      console.log('Search engine set to ' + searchEngine);
-    });
-  };  
